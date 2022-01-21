4fe3852d89a1361379c585cfc15a653844c3b5db
Marcos Vinicius <marcao.adsistema@gmail.com>
2022-01-20 22:47:36 -0300

    applying useeffect

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,5 +1,7 @@
-import React from "react";
 import { RepositoryItem } from "./RepositoryItem";
+
+import "../styles/repositories.scss";
+import { useEffect, useState } from "react";
 
 const repository = {
   name: "unform",
@@ -8,11 +10,20 @@ const repository = {
 };
 
 export const RepositoryList = () => {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/m4rcaoferole/repos")
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }, []);
+
   return (
     <section className="repo-list">
       <h1>Lista de Repositório</h1>
 
       <ul>
+        <RepositoryItem repository={repository} />
         <RepositoryItem repository={repository} />
         <RepositoryItem repository={repository} />
         <RepositoryItem repository={repository} />
